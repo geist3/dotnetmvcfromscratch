aecd25588898ec87ffe3ce2bb3a8baed57fe9d32
John Cockrell <jcockrell1@oxfam.org.uk>
2018-05-18 15:22:19 +0100

    Return a view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ namespace dotnetconsole {
     public class HomeController : Controller
     {
         public IActionResult Index(){
-            return Ok("Hello from controller");
+            return View();
         }
     }
 }
