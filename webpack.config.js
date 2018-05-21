94d3e8a06da786cfc0e5aa6b9a0f1d015c3546a8
John Cockrell <jcockrell1@ogb.internal.oxfam.net>
2018-05-21 16:23:05 +0100

    webpack config file, still run npm run wbp

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './wwwroot/source/app.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'bundle.js'
+    }
+}
