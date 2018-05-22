eea641317cbaad0440be3485244f58bf68179916
John Cockrell <jcockrell1@ogb.internal.oxfam.net>
2018-05-22 09:17:42 +0100

    require jquery through webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './wwwroot/source/app.js',
@@ -9,5 +10,12 @@ module.exports = {
     module: {
         rules: [ { test: /\.js?$/,
         use: { loader: 'babel-loader', options: { presets: ['babel-preset-env']}}}]
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'windows.jQuery': 'jquery'
+        })
+    ]
 }
--- wwwroot/source/app.js
+++ wwwroot/source/app.js
@@ -1,9 +1,9 @@
 require('./lib');
-import $ from 'jquery';
+//import $ from 'jquery';
 import ES6Lib from './es6codelib';
 //setText(document.getElementById("react-app"));
 //alert('a');
 let myES6Object = new ES6Lib();
 //alert(myES6Object.getData());
 
-$('#fillthiswithjquery').html('Filled by Jquery!');
+$('#fillthiswithjquery').html('Filled by Jquery!!');
